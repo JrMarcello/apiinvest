a64c6392816fed4dc0e46200e7e3996710025d10
Majun <marcello.dudk@gmail.com>
2019-07-20 20:53:12 -0300

    Commit inicial

--- web-api/src/common/utils.js
+++ web-api/src/common/utils.js
@@ -1,9 +1,12 @@
 import conf from '@core/configs'
 import * as log from '@core/logger'
+import uuid from 'uuid/v4'
 
 export const configs = () => conf
 
 export const logger = () => log
+
+export const generateUUID = () => uuid()
 
 // module.exports.configs = conf
 // module.exports.logger = log
