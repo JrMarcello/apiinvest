2ba2d728d7a6d1c4615661b4ff9e82ad7fd788e6
Majun <marcello.dudk@gmail.com>
2019-09-09 20:31:26 -0300

    Ajustando scripts de BD

--- db/create-db-local.sh
+++ db/create-db-local.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
  
-psql postgres://postgres:postgres@localhost:5432/postgres -f db/scripts/create-db.sql
-psql postgres://postgres:postgres@localhost:5432/buildinvest -f db/scripts/create-schema.sql     
-psql postgres://postgres:postgres@localhost:5432/buildinvest -f db/scripts/create-tables.sql
-psql postgres://postgres:postgres@localhost:5432/buildinvest -f db/scripts/insert-default-data.sql
+psql postgres://postgres:postgres@localhost:5432/postgres -f db/sql/create-db.sql
+psql postgres://postgres:postgres@localhost:5432/buildinvest -f db/sql/create-schema.sql     
+psql postgres://postgres:postgres@localhost:5432/buildinvest -f db/sql/create-tables.sql
+psql postgres://postgres:postgres@localhost:5432/buildinvest -f db/sql/insert-default-data.sql
