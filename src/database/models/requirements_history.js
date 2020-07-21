08ed6dc733f6c771c45a3013a954d65368555b59
Jeferson Kal Lyns <jefersonkallyns@outlook.com>
2020-07-20 21:39:13 -0300

    Salvando histórico de investimentos e corrigindo problemas diversos

---
+++ src/database/models/requirements_history.js
@@ -0,0 +1,49 @@
+export default (sequelize, DataTypes) => {
+    const RequirementsHistory = sequelize.define('RequirementsHistory',
+      {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+          },
+    
+          id_investor: {
+            type: DataTypes.UUID,
+            allowNull: false
+          },
+    
+          id_investment: {
+            type: DataTypes.UUID,
+            allowNull: false
+          },
+    
+          invested_other_platforms: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false
+          },
+    
+          self_declaration: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+          },
+    
+          use_terms: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+            allowNull: false
+          }
+      },
+      {
+        tableName: 'requirements_history'
+      }
+    )
+  
+    // Associations
+    RequirementsHistory.associate = models => {
+      RequirementsHistory.belongsTo(models.Investor, { foreignKey: 'id_investor', as: 'investor' })
+      RequirementsHistory.belongsTo(models.Investment, { foreignKey: 'id_investment', as: 'investment' })
+    }
+  
+    return RequirementsHistory
+  }
+  
